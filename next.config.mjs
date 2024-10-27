128d61665123bcdfa29dc39ba7d59615a390da9f
Nakiswen <c1hotvip@163.com>
2024-10-27 23:02:45 +0800

    解决别名问题

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, './app')
+        };
+        return config;
+    }
 };
 
 export default nextConfig;
